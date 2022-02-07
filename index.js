236a073fb9b0451dccf372e4f55609d6cf92960b
Suren <simoniansuren@gmail.com>
2022-02-07 10:21:28 +0400

    import csv

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-//// read and import  files;
-//const fs = require("fs");
-//const process = require("process");
-
+// read and import  files;
 const fs = require("fs");
- 
+const EventEmitter = require("events");
 
-let readableStream = fs.createReadStream("my-1.csv" );
- 
-readableStream.on("data", function(chunk){ 
+const readLine = require("readline");
+const process = require("process");
 
-    console.log(chunk);
+const readableStream = fs.createReadStream("my-3.csv");
+
+const rl = readLine.createInterface({
+  input: readableStream,
 });
 
-//let readableStream = fs.createReadStream("hello.txt", "utf8");
- 
-//let writeableStream = fs.createWriteStream("some.txt");
- 
-//readableStream.on("data", function(chunk){
-//    writeableStream.write(chunk);
-//});
+let importedArr = [];
+   let importedObj = {};
+
+
+rl.on("line", (line) => {
+   importedArr.push(line.split(","));
+}).on("close", () => {
+  let objArr = [];
+  let headers = importedArr.splice(0, 1)[0];
+  importedArr.forEach((item) => {
+    for (let i = 0; i < item.length - 1; i++) {
+      importedObj[headers[i]] = item[i];
+    }
+    objArr.push(importedObj);
+  })
+  console.log(objArr);
+  console.log(JSON.stringify(objArr));
+});
+
